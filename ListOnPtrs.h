ab46ba5909874dac45e7a09cf0955f079aa18470
Arman <ovakimian.al@phystech.edu>
2023-11-06 15:25:20 +0300

    consts

--- ListOnPtrs.h
+++ ListOnPtrs.h
@@ -44,18 +44,18 @@ void ListDump(ListType* list, const char* fileName,
                               const int   line);
 
 #define LIST_TEXT_DUMP(list) ListTextDump((list), __FILE__, __func__, __LINE__)
-void ListTextDump(ListType* list, const char* fileName,
-                                  const char* funcName,
-                                  const int   line);
+void ListTextDump(const ListType* list, const char* fileName,
+                                        const char* funcName,
+                                        const int   line);
 
-void ListGraphicDump(ListType* list);
+void ListGraphicDump(const ListType* list);
 
 ListErrors ListInsert(ListType* list, ListElemType* anchor, const int value,
                                                             ListElemType** insertedValPtr);
 ListErrors ListErase (ListType* list, ListElemType* anchor);
 
-ListElemType* ListGetHead(ListType* list);
-ListElemType* ListGetTail(ListType* list);
+ListElemType* ListGetHead(const ListType* list);
+ListElemType* ListGetTail(const ListType* list);
 
 
 #endif
